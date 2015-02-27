0226cad765eb7ec0815a9a3d275ca41e15cc6cf1
Mike Hargiss <Mike.Hargiss@powerfinancetexas.com>
2015-02-26 18:15:41 -0700

    add mycargus.core for .net

---
+++ dotNet/mycargus.core/ConsoleLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace mycargus.Core
+{
+    public class ConsoleLogger : ILogger
+    {
+        private static Mutex _mutex = new Mutex();
+
+        public void WriteEntry(ArrayList a_Entry)
+        {
+            _mutex.WaitOne();
+
+            IEnumerator line = a_Entry.GetEnumerator();
+            while (line.MoveNext())
+                Console.WriteLine(line.Current);
+
+            _mutex.ReleaseMutex();
+        }
+
+        public void WriteEntry(string a_Entry)
+        {
+            _mutex.WaitOne();
+
+            Console.WriteLine(a_Entry);
+
+            _mutex.ReleaseMutex();
+        }
+    }
+}
---
+++ dotNet/mycargus.core/FormLogger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Threading;
+
+namespace mycargus.Core
+{
+    public class FormLogger : ILogger
+    {
+        private static Mutex _mutex = new Mutex();
+        private FieldInfo _output;
+        public bool AppendToLog { get; set; }      
+
+        public FormLogger(FieldInfo a_FormFieldInfo, bool a_AppendToLog = true)
+        {
+            _output = a_FormFieldInfo;
+            AppendToLog = a_AppendToLog;
+        }
+
+
+
+        public void WriteEntry(ArrayList a_Entry)
+        {
+            _mutex.WaitOne();
+
+            IEnumerator line = a_Entry.GetEnumerator();
+
+            _mutex.ReleaseMutex();
+        }
+
+        public void WriteEntry(string a_Entry)
+        {
+            _mutex.WaitOne();
+
+
+
+            _mutex.ReleaseMutex();
+        }
+    }
+}
